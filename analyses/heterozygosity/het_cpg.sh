c723b84570c58badceae6c438335caf0fa40c3d9
patriciapecnerova <95229545+patriciapecnerova@users.noreply.github.com>
2023-10-09 13:41:02 +0200

    Create het_cpg.sh

---
+++ analyses/heterozygosity/het_cpg.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+output="results"
+bamlist="muskox_bamlist.txt"
+ref="muskox_pseudohap.gapcloser.v1.lenchecked.fasta"
+contigs="whitelist.scaffolds"
+sites="whitelist_cpg.sites"
+
+cat ${bamlist} | while read line
+do
+angsd -i "$line" -anc $ref -dosaf 1 -minMapQ 30 -minQ 30 -P 10 -out $output/$(basename "${line%.*}")_CPGfilt \
+-uniqueOnly 1 -remove_bads 1 -rf $contigs -sites $sites \
+-gl 2 -doMajorMinor 1 -doCounts 1 -setMinDepth 3
+angsd/misc/realSFS -fold 1 $output/$(basename "${line%.*}")_CPGfilt.saf.idx > $output/$(basename "${line%.*}")_CPGfilt_est.ml
+done
+
+mkdir all_CPGfilt
+
+awk '{print FILENAME, $0}' *_est.ml > all/all_CPGfilt_est.ml
+sed -i 's/_est.ml//g' all/all_CPGfilt_est.ml
